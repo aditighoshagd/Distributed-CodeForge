9759c3785027e17da6bf62cf415c2786519df62c
aditighoshagd <aditighoshagd@gmail.com>
2026-07-11 01:48:11 +0530

    Add authentication and shared common library

---
+++ account-service/src/main/java/com/cybernode/ai/distributed_codeforge/account_service/entity/User.java
@@ -0,0 +1,48 @@
+package com.cybernode.ai.distributed_codeforge.account_service.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.Instant;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    @Setter
+    private String username;
+    @Setter
+    private String password;
+    @Setter
+    private String name;
+
+    @Column(unique = true)
+    String stripeCustomerId;
+
+    @CreationTimestamp
+    private Instant createdAt;
+    @UpdateTimestamp
+    private Instant updatedAt;
+
+    private Instant deletedAt;
+
+    @PrePersist
+    void setCreatedAt(){
+        this.createdAt=Instant.now();
+        this.updatedAt=Instant.now();
+    }
+    @PreUpdate
+    void setUpdatedAt(){
+        this.updatedAt=Instant.now();
+    }
+}
